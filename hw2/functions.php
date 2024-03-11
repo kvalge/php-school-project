2d66971daa9ab4e4a1d2958e25ab6a9b484954c3
Katrin Valge <katrin.valge@mail.ee>
2024-03-11 20:15:00 +0200

    hw2 test solution.

---
+++ hw2/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+function displayEmployees() {
+    $employeeList = [];
+    $readData = fopen('hw2/employees.txt', 'r');
+
+    while (($line = fgets($readData)) !== false) {
+        $decodedName = urldecode($line);
+        $employeeList[] = $decodedName;
+    }
+
+    fclose($readData);
+
+    $employees = $employeeList;
+    include 'hw2/employee-list.php';
+}
+
+function displayTasks() {
+    $taskList = [];
+    $readData = fopen('hw2/tasks.txt', 'r');
+
+    while (($line = fgets($readData)) !== false) {
+        $decodedTask = urldecode($line);
+        $taskList[] = $decodedTask;
+    }
+
+    fclose($readData);
+
+    $tasks = $taskList;
+    include 'hw2/task-list.php';
+}
